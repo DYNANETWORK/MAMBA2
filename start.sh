ac17342c28892edb5bebe7f770a74be3a07b0aa0
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-15 13:39:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 echo """
                                
 """
-rm -rf python_support_zone
+rm -rf MAMBA_SUPPORT_ZONE
 git clone https://github.com/SUKHPAL443/MAMBA_SUPPORT_ZONE
 cd MAMBA_SUPPORT_ZONE
 python3 -m userbot
