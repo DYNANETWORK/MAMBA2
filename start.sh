4bcc1361868cab28b7c86326b0319bc8785a1057
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-15 10:10:36 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo """
+                               
+"""
+rm -rf python_support_zone
+git clone https://github.com/SUKHPAL443/MAMBA_SUPPORT_ZONE
+cd MAMBA_SUPPORT_ZONE
+python3 -m userbot
