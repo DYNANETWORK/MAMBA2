b70b5e4259f5d22f71e5fb4358394afaa22ba6d2
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-15 14:19:05 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,7 +8,7 @@ from var import Var
 from userbot.Config import Config
 from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 from userbot.utils import load_module
-from userbot import LOAD_PLUG, LOGS, PYTHONversion
+from userbot import LOAD_PLUG, LOGS, MAMBAversion
 from pathlib import Path
 import asyncio
 import glob
