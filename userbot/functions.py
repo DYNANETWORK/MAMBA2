3303d23425424b80aed875f0c5632d01a3a7171d
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-15 13:47:45 +0530

    Add files via upload

--- userbot/functions.py
+++ userbot/functions.py
@@ -1,17 +1,39 @@
-import random
-import string
+import requests , os, re	
+from bs4 import BeautifulSoup	
+from asyncio import sleep	
+from random import choice	
+from telethon import events	
+from emoji import get_emoji_regexp	
+from PIL import Image	
+
+from telethon.tl.types import Channel	
 
 
-# String Finder
-def dc_finder(s, first, last):
-    try:
-        start = s.index(first) + len(first)
-        end = s.index(last, start)
-        return s[start:end]
-    except ValueError:
-        return ""
+async def darkmusic(dark , QUALITY):	
+  search = dark	
+  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}	
+  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text	
+  soup = BeautifulSoup(html, 'html.parser')	
+  for link in soup.find_all('a'):	
+    if '/watch?v=' in link.get('href'):	
+        # May change when Youtube Website may get updated in the future.	
+        video_link = link.get('href') 	
+        break	
+  video_link =  'http://www.youtube.com/'+video_link	
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)		
+  os.system(command)	
 
 
-# Id Generator
-def id_generator(size=64, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size))
+async def darkmusicvideo(dark):	
+    search = dark	
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}	
+    html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text	
+    soup = BeautifulSoup(html, 'html.parser')	
+    for link in soup.find_all('a'):	
+        if '/watch?v=' in link.get('href'):	
+            # May change when Youtube Website may get updated in the future.	
+            video_link = link.get('href') 	
+            break    	
+    video_link =  'http://www.youtube.com/'+video_link	
+    command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  	
+    os.system(command)
